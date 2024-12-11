a122a239d242c0a08c9f9c36b5f4f4199955bea9
unset <unset>
2024-12-11 22:09:40 +0800

    Updates

---
+++ component---src-elegantstack-gatsby-blog-pages-pages-authors-jsx-a6442d9c63221b91c7da.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_elegantstack_gatsby_starter_flexiblog_medical=self.webpackChunk_elegantstack_gatsby_starter_flexiblog_medical||[]).push([[794],{48471:function(t,e,a){a.r(e);a(27378);var l=a(96334),n=a(74359);e.default=t=>{const e=t.data.allArticleCategory.nodes,a=t.data.allArticleTag.nodes,s=[].concat(e&&e.length>0?[{title:"主題",items:e}]:[]).concat(a&&a.length>0?[{title:"標籤",items:a}]:[]);return t.pageContext.mobileMenu=s,(0,n.tZ)(l.Ar,t)}}}]);
+//# sourceMappingURL=component---src-elegantstack-gatsby-blog-pages-pages-authors-jsx-a6442d9c63221b91c7da.js.map
